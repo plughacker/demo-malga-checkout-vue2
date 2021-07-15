7bf54af7780c7550517925f4aa31af658e034c8d
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-07-15 09:01:42 -0300

    feat (app): create vue2 example of Plug Checkout integration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@plug-checkout/core/loader";
 
 Vue.config.productionTip = false;
+
+Vue.config.ignoredElements = [/plug-\w*/, /checkout-\w*/];
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 new Vue({
   render: (h) => h(App),
